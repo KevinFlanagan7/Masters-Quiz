b14b5ce6cecc7a3deec19df7d1c9a1d23cd54ff9
KevinFlanagan7 <148767900+KevinFlanagan7@users.noreply.github.com>
2024-01-26 16:15:36 +0000

    Function: Added constants and fuctions to js.

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+/*Declaration of Global constants*/
+const startQuizRef = document.querySelector('#startquiz');
+const instructionsRef = document.querySelector('#instructions');
+const quizContainerRef = document.querySelector('#quiz-container');
+const quizQuestionRef = document.querySelector('#quiz-question');
+const nextBtnRef = document.querySelector('#next-btn');
+const resultsBtnRef = document.querySelector('#results-btn');
+const optionsAreaRef = document.querySelector('#options-area');
+const nameRef = document.querySelector("#name");
+const resultContainerRef = document.querySelector("#result-container");
+const userNameRef = document.querySelector("#username");
+const currentQuestionRef = document.querySelector("#current-question");
+const scoreRef = document.querySelector("#score");
+const usernameResultRef = document.querySelector("#username-result");
+const resultMessageRef = document.querySelector("#result-message");
+
+
+
+function startQuiz() {}
+
+function displayShuffledQuestion() {}
+
+function showOptions() {}
+
+function selectAnswer() {}
+
+function increaseScore() {}
+
