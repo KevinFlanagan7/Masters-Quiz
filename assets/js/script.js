4cc0206e0c91cccd025cabedc8faf9a51e8dd28b
KevinFlanagan7 <148767900+KevinFlanagan7@users.noreply.github.com>
2024-01-27 13:16:57 +0000

    Funct: Replaced functions with arrow fuctions and put event listeners into Dom content loader at end of js file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,8 +20,8 @@ let shuffledQuestions;
 let currentQuestionIndex;
 let score = 0;
 
-startQuizRef.addEventListener("click", startQuiz);
-function startQuiz() {
+
+const startQuiz = () => {
     if (nameRef.value === '') {
         alert("Please enter name before clicking Start Quiz button");
     } else {
@@ -34,12 +34,12 @@ function startQuiz() {
     }
 }
 
-function displayShuffledQuestion() {
+const displayShuffledQuestion = () => {
     resetState();
     showOptions(shuffledQuestions[currentQuestionIndex]);
 }
 
-function showOptions(question) {
+const showOptions = (question) => {
     currentQuestionRef.innerHTML = currentQuestionIndex + 1;
     quizQuestionRef.innerHTML = question.question;
     question.answers.forEach(answer => {
@@ -55,14 +55,14 @@ function showOptions(question) {
     });
 }
 
-function resetState() {
+const resetState = () => {
     nextBtnRef.classList.add('hide');
     while (optionsAreaRef.firstChild) {
         optionsAreaRef.removeChild(optionsAreaRef.firstChild);
     }
 }
 
-function selectAnswer(event) {
+const selectAnswer = (event) => {
     const selectedButton = event.target;
     const correctOption = selectedButton.dataset.correct === 'true';
     const allOptions = optionsAreaRef.children.length;
@@ -95,37 +95,10 @@ function selectAnswer(event) {
 }
 
 /*Functions to increase scores*/
-function increaseScore() {
+const increaseScore = () => {
     let oldScore = parseInt(scoreRef.innerHTML);
     scoreRef.innerHTML = ++oldScore;
 }
-
-/*Evenet Listenereer for Next button*/
-nextBtnRef.addEventListener('click', function () {
-    currentQuestionIndex++;
-    displayShuffledQuestion();
-});
-
-
-/*Evenet Listenereer for Results button*/
-resultsBtnRef.addEventListener('click', function () {
-    quizContainerRef.classList.add('hide');
-    resultContainerRef.classList.remove('hide');
-    usernameResultRef.innerHTML = nameRef.value;
-    const scorePercent = Math.round(100 * score / questions.length);
-    if (scorePercent >= 80) {
-        resultMessageRef.innerHTML = `Congratulations, you got ${scorePercent}%, you are a Masters champion!`;
-        crowdApplauseRef.play();
-    } else if (scorePercent >= 60) {
-        resultMessageRef.innerHTML = `Well done, you got ${scorePercent}%, you made the cut!`;
-    } else if (scorePercent >= 40) {
-        resultMessageRef.innerHTML = `Hard luck, you got ${scorePercent}%, you didn't make the cut!`;
-    } else if (scorePercent >= 0) {
-        resultMessageRef.innerHTML = `Unfortunatley, you only got ${scorePercent}%, they didn't really suit you!`;
-    }
-});
-
-
 
 fetch("assets/data/questions.json")
     .then(response => {
@@ -139,6 +112,28 @@ fetch("assets/data/questions.json")
 document.addEventListener("DOMContentLoaded", () => {
 
     nameRef.focus();
+    startQuizRef.addEventListener("click", startQuiz);
 
+    nextBtnRef.addEventListener('click', () => {
+        currentQuestionIndex++;
+        displayShuffledQuestion();
+    });
+
+    resultsBtnRef.addEventListener('click', () => {
+        quizContainerRef.classList.add('hide');
+        resultContainerRef.classList.remove('hide');
+        usernameResultRef.innerHTML = nameRef.value;
+        const scorePercent = Math.round(100 * score / questions.length);
+        if (scorePercent >= 80) {
+            resultMessageRef.innerHTML = `Congratulations, you got ${scorePercent}%, you are a Masters champion!`;
+            crowdApplauseRef.play();
+        } else if (scorePercent >= 60) {
+            resultMessageRef.innerHTML = `Well done, you got ${scorePercent}%, you made the cut!`;
+        } else if (scorePercent >= 40) {
+            resultMessageRef.innerHTML = `Hard luck, you got ${scorePercent}%, you didn't make the cut!`;
+        } else if (scorePercent >= 0) {
+            resultMessageRef.innerHTML = `Unfortunatley, you only got ${scorePercent}%, they didn't really suit you!`;
+        }
+    });
 });
 
